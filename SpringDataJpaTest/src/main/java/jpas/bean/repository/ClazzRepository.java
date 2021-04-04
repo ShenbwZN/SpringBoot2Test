bb3f86f256a1df76a150302a4b028e0f4518e6ef
申博文 <37896506+ShenbwZN@users.noreply.github.com>
2021-04-04 20:56:44 +0800

    Delete SpringDataJpaTest/src/main/java/jpas/bean/repository directory

--- SpringDataJpaTest/src/main/java/jpas/bean/repository/ClazzRepository.java
+++
@@ -1,9 +0,0 @@
-package jpas.repository;
-
-import jpas.bean.Clazz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClazzRepository extends JpaRepository<Clazz, String> {
-
-    Clazz findByName(String clazzName);
-}
